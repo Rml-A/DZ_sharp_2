23891d76bc86d2217c5bd6678f85c59d01249046
Ram <rmailtat777@gmail.com>
2023-02-08 21:15:12 +0300

    delo sdelano)

---
+++ Task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int newNumber = number % 100;
+
+System.Console.WriteLine(newNumber /10);
